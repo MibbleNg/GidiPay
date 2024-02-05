5038547d8f709caa8c090e6278750a2d9abfaea0
ahmadabdoul <44808233+ahmadabdoul@users.noreply.github.com>
2024-02-05 03:15:21 -0800

    added nativewind and assets

--- app/index.js
+++ app/index.js
@@ -1,21 +1,64 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SplashScreen, useRouter } from "expo-router";
 
-export default function App() {
+import React from "react";
+import { View, Image, Platform } from "react-native";
+import { getAuth } from "./helpers/auth";
+SplashScreen.preventAutoHideAsync();
+
+const blurhash =
+  "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
+
+export default function Splash() {
+  const router = useRouter();
+  const [isReady, setReady] = React.useState(false);
+  const [user, setUser] = React.useState([]);
+  React.useEffect(() => {
+    const authenticate = async () => {
+      // Perform some sort of async data or asset fetching.
+      let userAuth = await getAuth();
+      console.log(userAuth);
+      setUser(userAuth);
+      if (userAuth !== null) {
+        setTimeout(() => {
+          //console.log(user);
+          // When all loading is setup, unmount the splash screen component.
+          SplashScreen.hideAsync();
+          setReady(true);
+          router.replace("/customer");
+        }, 1000);
+      } else {
+        setTimeout(() => {
+          // When all loading is setup, unmount the splash screen component.
+          SplashScreen.hideAsync();
+          setReady(true);
+          router.replace("Landing");
+        }, 1000);
+      }
+    };
+    authenticate();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start</Text>
-      <Text>Open up App.js to start</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#00445",
+      }}
+    >
+      <Image
+        style={{
+          width: 200,
+          height: 200,
+          resizeMode: "contain",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        source={require("../assets/logo.png")}
+        contentFit="cover"
+        transition={1000}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
